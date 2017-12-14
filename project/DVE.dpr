24aae83216f6b7a6fa9933c2091d30d8ca5b8587
Thyandyr <32557323+Thyandyr@users.noreply.github.com>
2017-12-14 22:57:16 +0100

    vv
    bb

---
+++ project/DVE.dpr
@@ -0,0 +1,26 @@
+program DVE;
+
+uses
+  Vcl.Forms,
+  Neslib.Stb.Common in 'External\Neslib-Stb\Neslib.Stb.Common.pas',
+  Neslib.Stb.Image in 'External\Neslib-Stb\Neslib.Stb.Image.pas',
+  Neslib.Stb.TrueType in 'External\Neslib-Stb\Neslib.Stb.TrueType.pas',
+  dglOpenGL in 'External\dglOpenGL\dglOpenGL.pas',
+  UnitMain in 'UnitMain.pas' {FormTest},
+  dveOpenGLHandler in 'Source\dveOpenGLHandler.pas',
+  dveChunk in 'Source\dveChunk.pas',
+  dveChunkManager in 'Source\dveChunkManager.pas',
+  dveUnitSimplexNoise1234 in 'Source\dveUnitSimplexNoise1234.pas',
+  Neslib.Stb.RectPack in 'External\Neslib-Stb\Neslib.Stb.RectPack.pas',
+  Neslib.FastMath in 'External\FastMath\Neslib.FastMath.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  ReportMemoryLeaksOnShutdown := true;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormTest, FormTest);
+  Application.Run;
+end.
+
